fb0b24a59df774781a13a0b42edcc35aae3dd3f8
webdiziner01 <webdiziner01@gmail.com>
2018-06-27 18:24:16 +0500

    first commit on video 10

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('templates.default')
+
+
+@section('content')
+<h3>Welcome to Chatty</h3>
+The best social network, like... ever.
+@stop
---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', ['uses' => '\Chatty\Http\Controllers\HomeController@index',
+	'as' => 'home']
+);
+
+
+Route::get('/alert', function(){
+		return redirect()->route('home')->with('info', 'You have signed up');
+});
+Route::get('/signup',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@getSignup',
+	'as' => 'auth.signup'
+
+]);
+
+Route::post('/signup',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@postSignup'
+
+
+]);
+
+
+
+Route::get('/signin',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@getSignin',
+	'as' => 'auth.signin'
+
+]);
+
+Route::post('/signin',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@postSignin'
+
+
+]);
+
+
+Route::get('/signout',[
+	'uses' => '\Chatty\Http\Controllers\AuthController@getSignout',
+	'as' => 'auth.signout'
+
+]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
